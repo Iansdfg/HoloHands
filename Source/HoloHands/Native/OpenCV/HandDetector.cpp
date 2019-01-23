fdc32d8ddc10aa4310f94f762faa943837dc3100
Magnus Maynard <magnus.maynard@hotmail.com>
2019-01-23 22:31:09 +0000

    Refactoring code, improving performance.

--- Source/HoloHands/Native/OpenCV/HandDetector.cpp
+++ Source/HoloHands/Native/OpenCV/HandDetector.cpp
@@ -5,33 +5,37 @@
 #include "Converter.h"
 #include "Io/All.h"
 
-#include "opencv2/ml.hpp"
-
 using namespace HoloHands;
 using namespace Windows::Graphics::Imaging;
 using namespace cv;
 
-bool BoundingRectangleIntersects(std::vector<cv::Point>& a, std::vector<cv::Point>& b)
+void HandDetector::CalculateBounds(const std::vector<std::vector<Point>>& contours, std::vector<Rect>& bounds)
 {
-   auto rectA = boundingRect(a);
-   auto rectB = boundingRect(b);
-
-   return ((rectA & rectB).area() > 0);
+   bounds.clear();
+   for (auto& contour : contours)
+   {
+      bounds.push_back(boundingRect(contour));
+   }
 }
 
-void FilterContour(std::vector<cv::Point>& newContour, std::vector<std::vector<cv::Point>>& filteredContours)
+bool HandDetector::Intersects(const Rect& a, Rect& b)
 {
-   cv::Point2f center;
-   float radius;
-   minEnclosingCircle(newContour, center, radius);
+   return (a & b).area() > 0;
+}
 
+void HandDetector::FilterContour(
+   const std::vector<Point>& newContour,
+   const Rect& newBounds,
+   std::vector<std::vector<Point>>& filteredContours,
+   std::vector<Rect>& filteredBounds)
+{
    //Ignore small contours.
-   if (radius > 10)
+   if (newBounds.width > MIN_CONTOUR_SIZE && newBounds.height > MIN_CONTOUR_SIZE)
    {
-      int overlappingIndex = -1;
-      for (int i = 0; i < filteredContours.size(); i++)
+      size_t overlappingIndex = -1;
+      for (size_t i = 0; i < filteredContours.size(); i++)
       {
-         if (BoundingRectangleIntersects(newContour, filteredContours[i]))
+         if (Intersects(newBounds, filteredBounds[i]))
          {
             overlappingIndex = i;
             break;
@@ -40,49 +44,56 @@ void FilterContour(std::vector<cv::Point>& newContour, std::vector<std::vector<c
 
       if (overlappingIndex == -1)
       {
+         //Does not overlap.
          filteredContours.push_back(newContour);
+         filteredBounds.push_back(newBounds);
       }
       else
       {
+         //Merge overlapping contours.
          std::move(
             filteredContours[overlappingIndex].begin(),
             filteredContours[overlappingIndex].end(),
-            std::back_inserter(newContour));
+            std::back_inserter(const_cast<std::vector<Point>&>(newContour)));
+
+         //Calculate new bounding rect.
+         filteredBounds[overlappingIndex] = boundingRect(filteredContours[overlappingIndex]);
       }
    }
 }
 
-struct Defect
+void HandDetector::FilterContours(
+   const std::vector<std::vector<Point>>& rawCountours,
+   const std::vector<Rect>& rawBounds,
+   std::vector<std::vector<Point>>& filteredContours,
+   std::vector<Rect>& filteredBounds)
 {
-   Defect()
-      :
-      Depth(0)
-   {}
+   filteredContours.clear();
+   filteredBounds.clear();
 
-   cv::Point Start;
-   cv::Point End;
-   cv::Point Far;
-   float Depth;
-};
+   for (size_t i = 0; i < rawCountours.size(); i++)
+   {
+      FilterContour(rawCountours[i], rawBounds[i], filteredContours, filteredBounds);
+   }
+}
 
-Defect ExtractDefect(const std::vector<cv::Point>& contour, const cv::Vec4i& defectIndices)
+Defect HandDetector::ExtractDefect(const std::vector<Point>& contour, const Vec4i& defectIndices)
 {
    Defect defect;
-   defect.Start = cv::Point(contour[defectIndices[0]]);
-   defect.End = cv::Point(contour[defectIndices[1]]);
-   defect.Far = cv::Point(contour[defectIndices[2]]);
+   defect.Start = Point(contour[defectIndices[0]]);
+   defect.End = Point(contour[defectIndices[1]]);
+   defect.Far = Point(contour[defectIndices[2]]);
    defect.Depth = defectIndices[3] / 256.0f;
 
    return defect;
 }
 
-void HandDetector::Process(SoftwareBitmap^ input, cv::Mat& output)
+void HandDetector::Process(SoftwareBitmap^ input, Mat& output)
 {
    Mat original;
    Converter::Convert(input, original); //convert to opencv mat.
 
-   double max = 1000.0;
-   cv::Mat scaled = original / max * 255.0; //scale to within 8bit range.
+   Mat scaled = original / MAX_IMAGE_DEPTH * 255.0; //scale to within 8bit range.
 
    scaled.convertTo(scaled, CV_8UC1); //make correct format of opencv.
 
@@ -96,24 +107,26 @@ void HandDetector::Process(SoftwareBitmap^ input, cv::Mat& output)
    Mat cannyMat;// (original.cols, original.rows, CV_8UC1, Scalar(0));
    Canny(hands, cannyMat, 200, 250);
 
-   blur(cannyMat, cannyMat, cv::Size(6, 6));
+   blur(cannyMat, cannyMat, Size(6, 6));
 
    //Find contours.
-   std::vector<std::vector<cv::Point>> contours;
+   std::vector<std::vector<Point>> contours;
    findContours(cannyMat, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 
+   //Get rectangular bounds for contours.
+   std::vector<Rect> bounds;
+   CalculateBounds(contours, bounds);
+
    //Filter contours
-   std::vector<std::vector<cv::Point>> filteredContours;
-   for (auto contour : contours)
-   {
-      FilterContour(contour, filteredContours);
-   }
+   std::vector<std::vector<Point>> filteredContours;
+   std::vector<Rect> filteredBounds;
+   FilterContours(contours, bounds, filteredContours, filteredBounds);
 
    Mat hullMat(original.cols, original.rows, CV_8UC1, Scalar(0));
-
-   std::vector<std::vector<cv::Point>>hull(filteredContours.size());
+   std::vector<std::vector<Point>>hull(filteredContours.size());
    std::vector<std::vector<int>>hullIndices(filteredContours.size());
 
+   //Calculate hulls.
    for (int i = 0; i < filteredContours.size(); i++)
    {
       auto& contour = filteredContours[i];
@@ -121,11 +134,12 @@ void HandDetector::Process(SoftwareBitmap^ input, cv::Mat& output)
       convexHull(Mat(contour), hull[i]);
       convexHull(Mat(contour), hullIndices[i], false, false);
 
+      //Calculate defects for each hull.
       std::vector<Vec4i> defects;
       convexityDefects(contour, hullIndices[i], defects);
       Defect largestDefect;
 
-      for (size_t d = 1; d < defects.size(); d++) //Ignoring the first defect, as it happens to be wrong. Does this work?
+      for (size_t d = 1; d < defects.size(); d++) //TODO: Ignoring the first defect, as it is incorrect. Why?
       {
          Defect defect = ExtractDefect(contour, defects[d]);
 
@@ -135,13 +149,18 @@ void HandDetector::Process(SoftwareBitmap^ input, cv::Mat& output)
          }
       }
 
-      cv::Point midPoint = (largestDefect.Start + largestDefect.End) / 2.0;
-      cv::circle(hullMat, largestDefect.Start, 2, Scalar(150), 2);
-      cv::circle(hullMat, largestDefect.End, 2, Scalar(150), 2);
-      cv::circle(hullMat, largestDefect.Far, 3, Scalar(150), 2);
-      cv::circle(hullMat, midPoint, 3, Scalar(255), 2);
+      if (largestDefect.Depth > MIN_DEFECT_DEPTH)
+      {
+         //Draw hull defects.
+         Point midPoint = (largestDefect.Start + largestDefect.End) / 2.0;
+         circle(hullMat, largestDefect.Start, 2, Scalar(150), 2);
+         circle(hullMat, largestDefect.End, 2, Scalar(150), 2);
+         circle(hullMat, largestDefect.Far, 3, Scalar(150), 2);
+         circle(hullMat, midPoint, 3, Scalar(255), 2);
+      }
    }
 
+   //Draw hulls and contours.
    for (int i = 0; i < filteredContours.size(); i++)
    {
       drawContours(hullMat, filteredContours, i, Scalar(255));
--- Source/HoloHands/Native/OpenCV/HandDetector.h
+++ Source/HoloHands/Native/OpenCV/HandDetector.h
@@ -2,9 +2,45 @@
 
 namespace HoloHands
 {
+   struct Defect
+   {
+      Defect()
+         :
+         Depth(0)
+      {}
+
+      cv::Point Start;
+      cv::Point End;
+      cv::Point Far;
+      float Depth;
+   };
+
    class HandDetector
    {
    public:
       void Process(Windows::Graphics::Imaging::SoftwareBitmap^ bitmap, cv::Mat& outMat);
+
+   private:
+      const double MAX_IMAGE_DEPTH = 1000;
+      const double MIN_CONTOUR_SIZE = 10;
+      const double MIN_DEFECT_DEPTH = 20;
+
+      void CalculateBounds(const std::vector<std::vector<cv::Point>>& contours, std::vector<cv::Rect>& bounds);
+
+      bool Intersects(const cv::Rect& a, cv::Rect& b);
+
+      void FilterContour(
+         const std::vector<cv::Point>& newContour,
+         const cv::Rect& newBounds,
+         std::vector<std::vector<cv::Point>>& filteredContours,
+         std::vector<cv::Rect>& filteredBounds);
+
+      void FilterContours(
+         const std::vector<std::vector<cv::Point>>& rawCountours,
+         const std::vector<cv::Rect>& rawBounds,
+         std::vector<std::vector<cv::Point>>& filteredContours,
+         std::vector<cv::Rect>& filteredBounds);
+
+      Defect ExtractDefect(const std::vector<cv::Point>& contour, const cv::Vec4i& defectIndices);
    };
 }
